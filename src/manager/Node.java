309e60453cce838e2a25721070214ab842a8abf1
LancerTendy <korsun.lexa@mail.ru>
2025-01-05 17:21:44 +0700

    Node.java

--- src/manager/Node.java
+++ src/manager/Node.java
@@ -3,11 +3,37 @@ package manager;
 import tasks.Task;
 
 class Node {
-    Task task;
-    Node prev;
-    Node next;
+    private Task task; // Сделаем поле private для соблюдения инкапсуляции
+    private Node prev; // Предыдущий узел
+    private Node next; // Следующий узел
 
-    Node(Task task) {
+    // Конструктор
+    public Node(Task task) {
         this.task = task;
+    }
+
+    // Геттеры и сеттеры для полей
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
     }
 }
