20a9de8e0e90849fbb369453b67a08667e350c26
LancerTendy <korsun.lexa@mail.ru>
2025-01-17 20:42:42 +0700

    Update Epic.java

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -1,8 +1,28 @@
 package tasks;
 
-// tasks.Epic.java
+import java.util.ArrayList;
+import java.util.List;
+
 public class Epic extends Task {
+    private final List<Integer> subtaskIds; // Список идентификаторов подзадач
+
     public Epic(int id, String name) {
         super(id, name);
+        this.subtaskIds = new ArrayList<>(); // Инициализация списка подзадач
+    }
+
+    // Метод для добавления подзадачи
+    public void addSubtask(int subtaskId) {
+        subtaskIds.add(subtaskId);
+    }
+
+    // Метод для удаления подзадачи
+    public void removeSubtask(int subtaskId) {
+        subtaskIds.remove(Integer.valueOf(subtaskId));
+    }
+
+    // Метод для получения списка подзадач
+    public List<Integer> getSubtaskIds() {
+        return new ArrayList<>(subtaskIds); // Возвращаем копию списка подзадач
     }
 }
