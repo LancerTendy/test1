e373f59ffbec5f987c1249b123f1aa41309e727a
LancerTendy <korsun.lexa@mail.ru>
2025-01-17 20:47:51 +0700

    Update InMemoryHistoryManagerTest.java

--- src/test/InMemoryHistoryManagerTest.java
+++ src/test/InMemoryHistoryManagerTest.java
@@ -1,38 +1,44 @@
 package test;
 
+import manager.Manager;
 import manager.InMemoryHistoryManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import tasks.Task;
 
-import static org.junit.jupiter.api.Assertions.*;
-import java.util.List; // Добавьте этот импорт
+import java.util.List;
 
 public class InMemoryHistoryManagerTest {
 
+    private InMemoryHistoryManager historyManager;
+    private Task task1;
+    private Task task2;
+
+    @BeforeEach
+    public void setUp() {
+        historyManager = Manager.createHistoryManager(); // Создаем экземпляр через метод Manager
+        task1 = new Task(1, "Test Task 1");
+        task2 = new Task(2, "Test Task 2");
+    }
+
     @Test
     public void testAddAndGetHistory() {
-        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
-        Task task1 = new Task(1, "Test tasks.Task 1");
-        Task task2 = new Task(2, "Test tasks.Task 2");
-
         historyManager.add(task1);
         historyManager.add(task2);
 
         List<Task> history = historyManager.getHistory();
-        assertEquals(2, history.size());
+        Assertions.assertEquals(2, history.size());
         Assertions.assertEquals(task1.getId(), history.get(0).getId());
         Assertions.assertEquals(task2.getId(), history.get(1).getId());
     }
 
     @Test
     public void testRemoveTask() {
-        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
-        Task task1 = new Task(1, "Test tasks.Task 1");
         historyManager.add(task1);
 
         historyManager.remove(task1.getId());
         List<Task> history = historyManager.getHistory();
-        assertEquals(0, history.size());
+        Assertions.assertEquals(0, history.size());
     }
 }
