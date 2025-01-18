a457bcc6010c343aa4de2ae5e4ed19b0b3c9947f
LancerTendy <korsun.lexa@mail.ru>
2025-01-18 12:18:00 +0700

    Update Node.java

--- src/manager/Node.java
+++ src/manager/Node.java
@@ -1,13 +1,37 @@
-package manager;
+package tasks;
 
 import tasks.Task;
 
-class Node {
-    Task task;
-    Node prev;
-    Node next;
+// Класс для узла двусвязного списка
+public class Node {
+    private Task task; // Ссылка на задачу
+    private Node prev; // Ссылка на предыдущий узел
+    private Node next; // Ссылка на следующий узел
 
-    Node(Task task) {
+    public Node(Task task) {
         this.task = task;
+    }
+
+    // Геттер для задачи
+    public Task getTask() {
+        return task;
+    }
+
+    // Геттер и сеттер для предыдущего узла
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    // Геттер и сеттер для следующего узла
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
     }
 }
