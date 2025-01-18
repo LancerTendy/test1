f9c554953fd108a7f8a68e5ecbfcabbdb7fc0c79
LancerTendy <korsun.lexa@mail.ru>
2025-01-18 20:27:49 +0700

    Update Task.java

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -3,12 +3,21 @@ package tasks;
 public class Task {
     private int id;
     private String name;
-    private Status status; // Статус задачи
+    private TaskStatus status; // Статус задачи
+    private Integer epicId; // Для подзадач, если есть
 
     public Task(int id, String name) {
         this.id = id;
         this.name = name;
-        this.status = Status.NEW; // По умолчанию статус новой задачи
+        this.status = TaskStatus.NEW; // По умолчанию статус новой задачи
+        this.epicId = null; // По умолчанию подзадача не привязана к эпикам
+    }
+
+    public Task(int id, String name, int epicId) {
+        this.id = id;
+        this.name = name;
+        this.status = TaskStatus.NEW; // По умолчанию статус новой подзадачи
+        this.epicId = epicId; // Подзадача привязана к эпикам
     }
 
     public int getId() {
@@ -19,11 +28,15 @@ public class Task {
         return name;
     }
 
-    public Status getStatus() {
+    public TaskStatus getStatus() {
         return status;
     }
 
-    public void setStatus(Status status) {
+    public void setStatus(TaskStatus status) {
         this.status = status;
+    }
+
+    public Integer getEpicId() {
+        return epicId;
     }
 }
