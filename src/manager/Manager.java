55b20865c1567d6769d925a0f7958ae1ec62a57d
LancerTendy <korsun.lexa@mail.ru>
2025-01-18 20:18:01 +0700

    Update Manager.java

--- src/manager/Manager.java
+++ src/manager/Manager.java
@@ -1,28 +1,16 @@
 package manager;
 
 import tasks.Task;
-
 import java.util.List;
 
 public class Manager {
-    private final InMemoryHistoryManager historyManager;
-
-    public Manager() {
-        this.historyManager = new InMemoryHistoryManager(); // Инициализация менеджера истории
+    // Метод для создания и получения экземпляра InMemoryTaskManager
+    public static TaskManager createTaskManager() {
+        return new InMemoryTaskManager(); // Возвращаем экземпляр InMemoryTaskManager
     }
 
-    // Метод для добавления задачи
-    public void addTask(Task task) {
-        historyManager.add(task);
-    }
-
-    // Метод для удаления задачи по ID
-    public void removeTask(int id) {
-        historyManager.remove(id);
-    }
-
-    // Метод для получения истории задач
-    public List<Task> getTaskHistory() {
-        return historyManager.getHistory();
+    // Метод для создания и получения экземпляра InMemoryHistoryManager
+    public static InMemoryHistoryManager createHistoryManager() {
+        return new InMemoryHistoryManager(); // Возвращаем экземпляр InMemoryHistoryManager
     }
 }
