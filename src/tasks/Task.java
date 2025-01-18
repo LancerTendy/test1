423d01eee5fcc0161e71c3b07a8c99cad4c8d4d9
LancerTendy <korsun.lexa@mail.ru>
2025-01-18 20:28:50 +0700

    Update Task.java

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -1,42 +1,31 @@
 package tasks;
 
+// tasks.Task.java
 public class Task {
-    private int id;
-    private String name;
-    private TaskStatus status; // Статус задачи
-    private Integer epicId; // Для подзадач, если есть
+private int id;
+private String name;
+private Integer epicId; // Для подзадач, если есть
 
-    public Task(int id, String name) {
-        this.id = id;
-        this.name = name;
-        this.status = TaskStatus.NEW; // По умолчанию статус новой задачи
-        this.epicId = null; // По умолчанию подзадача не привязана к эпикам
-    }
+public Task(int id, String name) {
+    this.id = id;
+    this.name = name;
+    this.epicId = null; // По умолчанию подзадача не привязана к эпикам
+}
 
-    public Task(int id, String name, int epicId) {
-        this.id = id;
-        this.name = name;
-        this.status = TaskStatus.NEW; // По умолчанию статус новой подзадачи
-        this.epicId = epicId; // Подзадача привязана к эпикам
-    }
+public Task(int id, String name, int epicId) {
+    this.id = id;
+    this.name = name;
+    this.epicId = epicId; // Подзадача привязана к эпикам
+}
 
-    public int getId() {
-        return id;
-    }
+public int getId() {
+    return id;
+}
 
-    public String getName() {
-        return name;
-    }
+public String getName() {
+    return name;
+}
 
-    public TaskStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(TaskStatus status) {
-        this.status = status;
-    }
-
-    public Integer getEpicId() {
-        return epicId;
-    }
+public Integer getEpicId() {
+    return epicId;
 }
