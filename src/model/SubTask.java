21e1a16a2ab800f32ad83aa047e497ab2b6fad6a
LancerTendy <korsun.lexa@mail.ru>
2025-02-07 22:01:08 +0700

    Update SubTask.java

--- src/model/SubTask.java
+++ src/model/SubTask.java
@@ -3,21 +3,32 @@ package model;
 import java.util.Objects;
 
 public class SubTask extends Task {
-    private int epicId;
+    private int epicId; // ID эпика, к которому относится подзадача
 
-
+    // Конструктор
     public SubTask(String name, String description, int epicId) {
         super(name, description);
         this.epicId = epicId;
     }
 
+    // Геттер для epicId
     public Integer getEpicId() {
         return epicId;
     }
+
+    // Сеттер для epicId
+    public Integer setEpicId(int epicId) {
+        this.epicId = epicId;
+        return null; // Возвращает null, что может быть не лучшим решением. Рассмотрите возвращение void.
+    }
+
+    // Переопределение метода toString
     @Override
     public String toString() {
         return super.toString() + " EpicId: " + epicId;
     }
+
+    // Переопределение метода equals
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -29,13 +40,10 @@ public class SubTask extends Task {
         SubTask subTask = (SubTask) obj;
         return super.equals(obj) && epicId == subTask.epicId;
     }
+
+    // Переопределение метода hashCode
     @Override
     public int hashCode() {
         return Objects.hash(super.hashCode(), epicId);
     }
-    public Integer setEpicId(int epicId) {
-        this.epicId = epicId;
-        return null;
-    }
 }
-
